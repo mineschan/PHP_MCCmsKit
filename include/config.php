678d2cfb02adcd1f7eb8484ec6e0d84ff9e2318d
mineschan <mineschan@gmail.com>
2013-04-09 18:14:50 +0800

    Added Config file

---
+++ include/config.php
@@ -0,0 +1,18 @@
+<?
+
+session_start();
+
+//connect
+include_once("MCCmsKit/database/db.conn.php");
+include_once("MCCmsKit/database/db.class.php");
+
+//function class
+include_once("MCCmsKit/function/fc.common.php");
+
+/*****************
+**MYSQL Settings**
+*****************/
+define('DB_HOST', "$your_db_host");
+define('DB_USER',"$your_db_user");
+define('DB_PWD',"$your_db_pwd");
+define('DB_NAME',"$your_db_name");
