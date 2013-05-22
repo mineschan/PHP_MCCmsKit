193f20397b9b35c5060af661ded89afcbafb7c21
mineschan <mineschan@gmail.com>
2013-05-22 13:20:25 +0800

    Created init.php

---
+++ include/init.php
@@ -0,0 +1,25 @@
+$loader = ModelLoader::LoadDir(MODEL_DIR,NULL,"Model_",1);
+
+
+foreach($loader as $file){
+
+  $fp = fopen(MODEL_DIR.$file, 'r');
+	$class = $buffer = '';
+	$i = 0;
+	while (!$class) {
+	    if (feof($fp)) break;
+	
+	    $buffer .= fread($fp, 512);
+	    if (preg_match('/class\s+(\w+)(.*)?\{/', $buffer, $matches)) {
+	        $class = $matches[1];
+	        break;
+	    }
+	}
+	
+	$$class = new $class();
+	define($class,$$class);
+	
+	if($class == get_class($$class)){
+		$created[] = $class;
+	}
+}
