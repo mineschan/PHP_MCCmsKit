4d28db49e3682a65156039446a673a31e69bb63e
mineschan <mineschan@gmail.com>
2013-05-22 13:22:18 +0800

    Updated config.php

--- include/config.php
+++ include/config.php
@@ -17,3 +17,8 @@ define('DB_HOST', "$your_db_host");
 define('DB_USER',"$your_db_user");
 define('DB_PWD',"$your_db_pwd");
 define('DB_NAME',"$your_db_name");
+
+/*****************
+**Database Model Settings**
+*****************/
+define("MODEL_DIR",$_SERVER["DOCUMENT_ROOT"] . $path_to_your_model_dir); //for model loader
