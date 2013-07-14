3d8661ce412e400329f2a66c0774acadcb684238
mineschan <mineschan@gmail.com>
2013-07-14 17:39:19 +0800

    Update db_conn.php

--- include/database/db_conn.php
+++ include/database/db_conn.php
@@ -1,1 +1,12 @@
-1
+<?
+  function conn(){
+		$conn = mysql_connect(DB_HOST,DB_USER,DB_PWD);
+				
+		$select_db = mysql_select_db(DB_NAME);
+		mysql_query("SET NAMES 'utf8'"); 
+        mysql_query("SET CHARACTER_SET_CLIENT=utf8"); 
+        mysql_query("SET CHARACTER_SET_RESULTS=utf8");
+         			
+		if(!$conn || !$select_db)
+			echo "<script>alert('Connect to Database error')</script>";	
+  }
